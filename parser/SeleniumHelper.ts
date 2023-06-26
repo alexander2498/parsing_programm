f2f30d44615a9a5cfb8d5a895d09b298b46e3c17
phpis <p.shpis@inbox.ru>
2023-06-27 03:19:50 +0400

    feat: prepare method which return full html with needed data

--- parser/SeleniumHelper.ts
+++ parser/SeleniumHelper.ts
@@ -2,9 +2,19 @@ import {Builder, By, ThenableWebDriver} from 'selenium-webdriver';
 import * as chrome from "selenium-webdriver/chrome";
 import fs from "fs";
 
+export const allHelpers: Array<SeleniumHelper> = [];
+
+export const destructAllHelpers = async () => {
+  for (const helper of allHelpers) {
+    await helper.destruct();
+  }
+};
+
 export class SeleniumHelper {
   private chromeOptions: chrome.Options;
   private driver: ThenableWebDriver;
+  private baseUrl: string;
+
 
   constructor() {
     this.chromeOptions = new chrome.Options()
@@ -19,10 +29,67 @@ export class SeleniumHelper {
       .usingServer('http://109.172.80.51:4444/')
       .setChromeOptions(this.chromeOptions)
       .build();
+
+    this.baseUrl = 'https://msk.beauty.firmika.ru/';
+
+    allHelpers.push(this);
   }
 
   destruct() {
     return this.driver.quit();
+  }
+
+  public async prepareDistrictPage (url: string) {
+    try {
+      url = this.baseUrl + url;
+      await this._openUrl(url);
+
+      for (let i = 0; i < 100; i++) {
+
+        try {
+          const moreBtn =  this.driver.findElement(By.css('.button.button--transparent.button--big.btn.showMoreFirms.ld'));
+
+          await moreBtn.click();
+          await this.driver.sleep(2000);
+
+          console.log(await moreBtn.getText());
+        }
+        catch (e) {
+          break;
+        }
+      }
+
+      await this._saveHtml('districtPage.html');
+
+      return await this.driver.getPageSource();
+
+    }
+    catch (e) {
+      console.log(e);
+      await this.destruct();
+      console.log('Error caught');
+    }
+  }
+
+  public async prepareSalonPage (url: string) {
+    try {
+      url = this.baseUrl + url;
+      await this._openUrl(url);
+
+      const phoneBtn = this.driver.findElement(By.css('.button.button--small.button--blue.buttonShowPhone'))
+      await phoneBtn.click();
+
+      await this.driver.sleep(2000);
+
+      await this._saveHtml('salonPage.html');
+      return await this.driver.getPageSource();
+
+    }
+    catch (e) {
+      console.log(e);
+      await this.destruct();
+      console.log('Error caught');
+    }
   }
 
   private _getHashCode(s: string) {
@@ -46,27 +113,24 @@ export class SeleniumHelper {
     await fs.promises.writeFile(prefix + '_screenshot.png', img, 'base64');
   }
 
-  public async test() {
-    try {
-      const url = 'https://msk.beauty.firmika.ru/p_a2_nav_districts'
-      await this._openUrl(url);
-
-      const element = await this.driver.findElement(By.className('.item.area'));
-
-      console.log(element);
-    }
-    catch (e) {
-      console.log(e);
-      await this.destruct();
-      console.log('Error caught');
-    }
-
+  private async _saveHtml(filename: string) {
+    const page = await this.driver.getPageSource();
+    await fs.promises.writeFile(filename, page, 'utf8');
   }
+
+
 
 }
 
 
 const seleniumHelper = new SeleniumHelper();
-seleniumHelper.test();
+// seleniumHelper.prepareDistrictPage('/districts.php?district=19').then(() => {
+//   seleniumHelper.destruct();
+// });
 
+seleniumHelper.prepareSalonPage('firms.php?i=19031').then(() => {
+  seleniumHelper.destruct();
+});
 
+process.once('SIGINT', () => destructAllHelpers());
+process.once('SIGTERM', () => destructAllHelpers());
